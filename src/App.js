4d4a08db9b9dc6cd42daf10b5f1bec6e7ed70919
Jonathan Jacka <jonathanjacka@gmail.com>
2023-08-03 13:16:48 -0500

    Updated links and styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import './App.css';
 import { NavBar } from './components/NavBar';
-import { Header } from './components/Header';
-import { BrowserRouter } from 'react-router-dom';
+import { Cards } from './components/Cards';
+import { Wallet } from './components/Wallet';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Footer } from './components/Footer';
-
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <>
-      <NavBar />
-      <div className='header'>
-      <BrowserRouter>
-      <Header />
-      </BrowserRouter>
-      </div>
+    <GlobalProvider>
+      <Router>
+        <NavBar />
+        <div className='header'>
+          <Routes>
+            <Route path='/' element={<Cards />} />
+            <Route path='/wallet' element={<Wallet />} />
+          </Routes>
+        </div>
+      </Router>
       <Footer />
-    </>
+    </GlobalProvider>
   );
 }
 
